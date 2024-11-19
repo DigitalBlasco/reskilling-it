f14751b5ff6adccea716205b95fc4460b2b72c7f
Roser Blasco <roser.blasco@gmail.com>
2024-11-19 13:42:37 +0100

    Nivell 1 - Exercici 1

---
+++ 1.1.sql
@@ -0,0 +1,12 @@
+# Nivell 1
+# Exercici 1
+
+# A partir dels documents adjunts (estructura_dades i dades_introduir), importa les dues taules. 
+# Mostra les característiques principals de l'esquema creat i explica les diferents taules i variables que existeixen. 
+# Assegura't d'incloure un diagrama que il·lustri la relació entre les diferents taules i variables.
+
+
+SELECT * FROM transactions.transaction;
+
+SELECT * FROM transactions.company;
+
