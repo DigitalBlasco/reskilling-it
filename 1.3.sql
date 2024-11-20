749cdcbf032c79773908566d33a05e4b33ea44ac
Roser Blasco <roser.blasco@gmail.com>
2024-11-20 15:44:41 +0100

    Amb l'exercici 3.2

--- 1.3.sql
+++ 1.3.sql
@@ -5,4 +5,14 @@ WHERE company_id in (
 	FROM company 
 	WHERE country ='Germany');
 
+Select company_name
+FROM company
+WHERE id in ( 
+	SELECT distinct company_id
+	FROM transaction
+	WHERE amount > (
+		SELECT avg(amount)
+		FROM transaction) 
+	)
+ORDER by company_name;
 
