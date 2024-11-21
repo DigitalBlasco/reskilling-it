ca45501caf1758dbe0ede89df753c88c3ed1bd96
Roser Blasco <roser.blasco@gmail.com>
2024-11-21 10:26:23 +0100

    Nivell 1 - Exercici 3

--- 1.3.sql
+++ 1.3.sql
@@ -1,9 +1,17 @@
 Select *
+### NIVELL 1 ###
+
+## EXERCICI 3
+
+# - Mostra totes les transaccions realitzades per empreses d'Alemanya.
+
 FROM transaction
 WHERE company_id in (
 	SELECT id
 	FROM company 
 	WHERE country ='Germany');
+
+#  - Llista les empreses que han realitzat transaccions per un amount superior a la mitjana de totes les transaccions.
 
 Select company_name
 FROM company
@@ -16,6 +24,7 @@ WHERE id in (
 	)
 ORDER by company_name;
 
+# - Eliminaran del sistema les empreses que no tenen transaccions registrades, entrega el llistat d'aquestes empreses.
 Select company_name
 FROM company
 WHERE id NOT in ( 
