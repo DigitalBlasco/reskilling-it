ff4d9157d418869c4560bb252401df7f2bb27770
Roser Blasco <roser.blasco@gmail.com>
2024-11-21 10:25:34 +0100

    Nivell 1 - Exercici 2

---
+++ 1.2.sql
@@ -0,0 +1,26 @@
+### NIVELL 1 ###
+
+## EXERCICI 2
+
+# - Llistat dels països que estan fent compres.
+SELECT distinct country
+FROM company
+LEFT JOIN transaction
+	ON company.id = transaction.company_id
+
+ORDER BY country ASC;
+
+# - Des de quants països es realitzen les compres.
+SELECT count(distinct country)
+FROM company
+LEFT JOIN transaction
+	ON company.id = transaction.company_id;
+    
+# - Identifica la companyia amb la mitjana més gran de vendes.
+SELECT company.company_name, avg(amount)
+FROM company
+LEFT JOIN transaction
+	ON company.id = transaction.company_id
+GROUP BY company.company_name
+ORDER BY avg(amount) DESC
+LIMIT 1;   
