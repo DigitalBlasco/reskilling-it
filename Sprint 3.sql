f75decb8838e882726f2f3b876aaba2d3932f1fa
Roser Blasco <roser.blasco@gmail.com>
2024-11-25 16:42:37 +0100

    N1E1

---
+++ Sprint 3.sql
@@ -0,0 +1,49 @@
+### NIVELL 1 ###
+## EXERCICI 1 ##
+
+SHOW TABLES;
+
+-- Creem la taula credit_card
+CREATE TABLE IF NOT EXISTS credit_card (
+	id VARCHAR(15) PRIMARY KEY,
+	iban VARCHAR(255),
+	pan VARCHAR(255),
+	pin VARCHAR(4),
+	cvv VARCHAR(3),
+	expiring_date VARCHAR(255)
+);
+
+SHOW TABLES;
+
+
+
+
+
+
+
+
+
+
+
+
+### NIVELL 1 ###
+## EXERCICI 2 ##
+
+### NIVELL 1 ###
+## EXERCICI 3 ##
+
+### NIVELL 1 ###
+## EXERCICI 4 ##
+
+
+### NIVELL 2 ###
+## EXERCICI 1 ##
+
+
+### NIVELL 2 ###
+## EXERCICI 2 ##
+
+
+### NIVELL 2 ###
+## EXERCICI 3 ##
+
