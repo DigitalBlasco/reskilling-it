8b576c59561d1ce3b0810f1f681f9d0bd21a4db8
Roser Blasco <roser.blasco@gmail.com>
2024-11-25 11:42:36 +0100

    Add files via upload

--- Sprint2.sql
+++ Sprint2.sql
@@ -16,7 +16,7 @@ SELECT * FROM transactions.company;
 # - Llistat dels països que estan fent compres.
 SELECT distinct country
 FROM company
-LEFT JOIN transaction
+RIGHT JOIN transaction
 	ON company.id = transaction.company_id
 
 ORDER BY country ASC;
@@ -24,17 +24,18 @@ ORDER BY country ASC;
 # - Des de quants països es realitzen les compres.
 SELECT count(distinct country)
 FROM company
-LEFT JOIN transaction
+RIGHT JOIN transaction
 	ON company.id = transaction.company_id;
     
 # - Identifica la companyia amb la mitjana més gran de vendes.
 SELECT company.company_name, avg(amount)
 FROM company
-LEFT JOIN transaction
+RIGHT JOIN transaction
 	ON company.id = transaction.company_id
 	WHERE declined = 0
 GROUP BY company.company_name
-ORDER BY avg(amount) DESC;   
+ORDER BY avg(amount) DESC
+LIMIT 1;   
 
 
 ### NIVELL 1 ###
